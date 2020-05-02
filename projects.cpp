443120a1dceebf7ed228aff69e83ac45c882011c
Liv Campos <livlucampos@gmail.com>
2020-05-02 18:27:31 +0100

    projects - ACC, sorting was updated to O(n * log(n)) with a map and array. Method had to be changed to O(n * log(n)) algorithm since n^2 is too slow. This included using a binary search

--- projects.cpp
+++ projects.cpp
@@ -11,11 +11,11 @@
 #include <stdio.h>
 
 using namespace std;
-
+/*
 bool cmp(int x, int y) {
     return e[x] < e[y];
 }
-
+*/
 long long int noProjects;
 long long int maxReward;
 
@@ -25,14 +25,42 @@ int main() {
     int startTimes[noProjects + 1];
     int endTimes[noProjects + 1];
     long long int rewards[noProjects + 1];
+
+    long long int dp[noProjects + 1];
+    long long int aggregatedReward[noProjects + 1];
+    long long int maxAggregatedReward[noProjects + 1];
+
+    map <pair <int, int>, int> sortingByEndAndStartTime;
+
+    maxAggregatedReward[0] = 0;
     for (int i = 1; i <= noProjects; i++) {
         cin >> startTimes[i];
         cin >> endTimes[i];
         cin >> rewards[i];
+
+        sortingByEndAndStartTime[{endTimes[i], i}] = i;
+
+        dp[i] = 1;
+        aggregatedReward[i] = rewards[i]; //because the minimum reward a project will have is it's current reward
+        maxAggregatedReward[i] = 0;
     }
 
-    int n = sizeof(startTimes)/sizeof(startTimes[0]);
-    sort(startTimes, startTimes + n, sortByStart(1, startTimes, endTimes, rewards));
+    int order[noProjects + 1];
+    int ind = 0;
+    for (auto itr = sortingByEndAndStartTime.begin(); itr != sortingByEndAndStartTime.end(); ++itr) {
+        order[++ind] = itr->second;
+        //cout << order[itr->second] << " ";
+        //cout << itr->first.first << " " << itr->first.second << endl;
+    }
+    //cout << endl;
+
+   // for (int i = 1; i <= noProjects; i++) {
+        //cout << order[i] << " " << endTimes[order[i]] << endl;
+   // }
+    //cout << endl;
+
+//    int n = sizeof(startTimes)/sizeof(startTimes[0]);
+ //   sort(startTimes, startTimes + n, sortByStart(1, startTimes, endTimes, rewards));
 /*
     while (1) {
         for (int i = 0; i < noProjects - 1; i++) {
@@ -63,38 +91,51 @@ int main() {
             break;
         }
     }
-*/
+
+
     for (int i = 1; i <= noProjects; i++) {
-        cout << startTimes[i] << " ";
-        cout << endTimes[i] << " ";
-        cout << rewards[i] << " ";
+        cout << startTimes[order[i]] << " ";
+        cout << endTimes[order[i]] << " ";
+        cout << rewards[order[i]] << " ";
         cout << endl;
     }
 
-    long long int dp[noProjects + 1];
-    int aggregatedReward[noProjects + 1];
-    for (int i = 1; i <= noProjects; i++) {
-        dp[i] = 1;
-        aggregatedReward[i] = rewards[i]; //because the minumum reward a project will have is it's current reward
-    }
-    int latestAddedReward = 0;
-
-
-
     for (int i = 1; i <= noProjects; i++) {
         for (int j = 1; j < i; j++) {
-            if (endTimes[j] < startTimes[i]) {
-                if (aggregatedReward[i] < aggregatedReward[j] + rewards[i]) {
-                    aggregatedReward[i] = aggregatedReward[j] + rewards[i];
+            if (endTimes[order[j]] < startTimes[order[i]]) {
+                if (aggregatedReward[order[i]] < aggregatedReward[order[j]] + rewards[order[i]]) {
+                    aggregatedReward[order[i]] = aggregatedReward[order[j]] + rewards[order[i]];
                 }
             }
         }
-        if (aggregatedReward[i] > maxReward) {
-            maxReward = aggregatedReward[i];
+        if (aggregatedReward[order[i]] > maxReward) {
+            maxReward = aggregatedReward[order[i]];
         }
     }
         cout << maxReward << endl;
+*/
 
-        return 0;
+
+    for (int i = 1; i <= noProjects;  i++) {
+        int l = 1;
+        int r = i - 1;
+        int answer = 0;
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (endTimes[order[m]] < startTimes[order[i]]) {
+                answer = m;
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+
+        aggregatedReward[order[i]] = maxAggregatedReward[answer] + rewards[order[i]];
+        maxAggregatedReward[i] = max(maxAggregatedReward[i - 1], aggregatedReward[order[i]]);
     }
+
+    cout << maxAggregatedReward[noProjects];
+
+    return 0;
+}
 
