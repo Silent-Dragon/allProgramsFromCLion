22d54afb72bf2be4f7f3b4f072b4dd480b83fe6c
Liv Campos <haris.theodoulou@shortcut.city>
2020-05-13 15:55:22 +0100

    labyrinth - correct solution, but gets TLE on big cases. Need to optimize in some way

--- labyrinth.cpp
+++ labyrinth.cpp
@@ -19,42 +19,68 @@ int endJ = 0;
 int cnt = 0;
 //vector <int> route;
 
+//vector <char> possibleDirections[1001][1001];
 
 void printPaths(int i, int j) {
     if (i == endI && j == endJ) {
-        cout << "Reached end with path length " << shortestPathLength[i][j];
+        cout << "Reached end with path length " << shortestPathLength[i][j] << endl;
         cnt++;
         return;
     }
 
-    if (i + 1 < n && a[i + 1][j] == '.') {
+    if (shortestPathLength[i][j] > shortestPathLength[endI][endJ]) {
+        cout << "Path is too long" << endl;
+        return;
+    }
+/*
+    if (a[i][j + 1] != '#') {
+        possibleDirections[i][j].push_back('R');
+    }
+
+    if (a[i][j - 1] != '#') {
+        possibleDirections[i][j].push_back('L');
+    }
+
+    if (a[i + 1][j] != '#') {
+        possibleDirections[i][j].push_back('D');
+    }
+
+    if (a[i - 1][j] != '#') {
+        possibleDirections[i][j].push_back('U');
+    }
+*/
+    if (i + 1 <= n && a[i + 1][j] != '#') {
         if (shortestPathLength[i + 1][j] > shortestPathLength[i][j] + 1) {
             shortestPathLength[i + 1][j] = shortestPathLength[i][j] + 1;
             cout << "Shortest path length for " << i + 1 << " " << j << " is " << shortestPathLength[i + 1][j] << endl;
+            cout << "D" << endl << endl;
             printPaths(i + 1, j);
         }
     }
 
-    if (j + 1 < m && a[i][j + 1] == '.') {
+    if (j + 1 <= m && a[i][j + 1] != '#') {
         if (shortestPathLength[i][j + 1] > shortestPathLength[i][j] + 1) {
             shortestPathLength[i][j + 1] = shortestPathLength[i][j] + 1;
             cout << "Shortest path length for " << i << " " << j + 1 << " is " << shortestPathLength[i][j + 1] << endl;
+            cout << "R" << endl << endl;
             printPaths(i, j + 1);
         }
     }
 
-    if (i - 1 < n && a[i - 1][j] == '.') {
+    if (i - 1 > 0 && a[i - 1][j] != '#') {
         if (shortestPathLength[i - 1][j] > shortestPathLength[i][j] + 1) {
             shortestPathLength[i - 1][j] = shortestPathLength[i][j] + 1;
             cout << "Shortest path length for " << i - 1 << " " << j << " is " << shortestPathLength[i - 1][j] << endl;
+            cout << "U" << endl << endl;
             printPaths(i - 1, j);
         }
     }
 
-    if (j - 1 < n && a[i][j - 1] == '.') {
+    if (j - 1 > 0 && a[i][j - 1] != '#') {
         if (shortestPathLength[i][j - 1] > shortestPathLength[i][j] + 1) {
             shortestPathLength[i][j - 1] = shortestPathLength[i][j] + 1;
             cout << "Shortest path length for " << i << " " << j - 1 << " is " << shortestPathLength[i][j - 1] << endl;
+            cout << "L" << endl << endl;
             printPaths(i, j - 1);
         }
     }
@@ -62,15 +88,14 @@ void printPaths(int i, int j) {
 
 
 int main() {
-    cin >> n;
-    cin >> m;
+    cin >> n >> m;
 
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; j++) {
             cin >> a[i][j];
-            cout << a[i][j] << " ";
+          //  cout << a[i][j] << " ";
 
-            shortestPathLength[i][j] = 100000;
+            shortestPathLength[i][j] = 1e6 + 1;
 
             if (a[i][j] == 'A') {
                 startI = i;
@@ -82,7 +107,7 @@ int main() {
                 endJ = j;
             }
         }
-        cout << endl;
+       //cout << endl;
     }
     cout << startI << " " << startJ << endl;
     cout << endI << " " << endJ << endl;
@@ -96,8 +121,9 @@ int main() {
         cout << "NO" << endl;
     } else {
         cout << "YES" << endl;
-        cout << "Shortest path length " << shortestPathLength[endI][endJ] << endl;
+        cout << shortestPathLength[endI][endJ] << endl;
     }
+
 
     return 0;
 }
