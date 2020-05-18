84edc2446fd1ec6fdfc903b52357cdde1eda093f
Liv Campos <livlucampos@gmail.com>
2020-05-18 20:04:59 +0200

    using this testing program to build the holy table

---
+++ TEST.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <map>
+#include <stack>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+#define pb push_back
+
+vector <int> v;
+
+int main() {
+
+    int i = 0;
+
+    vector <int> myVector;
+    for (int f = i; f < 2; f++) {
+        cout << f << " ";
+    }
+
+    int a = 2;
+
+    cout << ceil(2);
+
+    return 0;
+}
