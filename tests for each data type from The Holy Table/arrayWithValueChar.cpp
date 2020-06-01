03fd6470a17f3fbb579d2a280500ab689719e4d9
Liv Campos <livlucampos@gmail.com>
2020-06-01 18:07:14 +0200

    actions for array with value char

---
+++ tests for each data type from The Holy Table/arrayWithValueChar.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <map>
+#include <stack>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <iterator>
+
+
+using namespace std;
+
+///file for all columns for array with index of type int
+int main() {
+
+    int n = 5;
+
+    ///init
+    char myArray[n];
+
+    ///adding values
+    myArray[0] = 'h';
+    myArray[1] = 'i';
+    myArray[2] = 'G';
+    myArray[3] = 'Q';
+    myArray[4] = 'j';
+
+    ///iterating and printing
+    for (int i = 0; i < n; i++) {
+        ///index and value
+        cout << i << " ";
+        cout << myArray[i] << " ";
+    }
+    cout << endl;
+
+    ///sort
+    sort(myArray, myArray + n);
+
+    ///iterating and printing
+    for (int i = 0; i < n; i++) {
+        ///value
+        cout << myArray[i] << " ";
+    }
+    cout << endl;
+
+    ///start and end
+    cout << myArray[0] << " " << myArray[n - 1] << endl;
+
+    ///min and max elements
+    cout << *min_element(myArray, myArray + n) << " " << *max_element(myArray, myArray + n) << endl;
+
+    ///find by index and value
+    int wantedIndex = 4;
+    int wantedValue = 66;
+    cout << myArray[4] << " " << find(myArray, myArray + n, wantedValue) << endl;
+
+    ///upper and lower bound - KEEP IN MIND THAT ARRAY HAS BEEN SORTED
+    cout << upper_bound(myArray, myArray + n, wantedValue) << " ";
+    cout << lower_bound(myArray, myArray + n, wantedValue) << endl;
+
+    ///fin
+
+    return 0;
+}
