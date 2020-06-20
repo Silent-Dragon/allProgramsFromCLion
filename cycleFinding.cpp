c4a41a088e265e95d2865eddfca184d438d0a871
Liv Campos <haris.theodoulou@shortcut.city>
2020-06-20 17:55:27 +0100

    ACC and works but not for case which will be described in next commit

--- cycleFinding.cpp
+++ cycleFinding.cpp
@@ -14,6 +14,7 @@ using namespace std;
 int n = 0;
 int m = 0;
 vector <pair <int, int>> graph[2501];
+vector <int> cyclePath;
 long long int dis[2501];
 int parentNode[2501];
 //bool updated[2501]; we only need the last updated node, not all of the ones that are updated
@@ -21,18 +22,16 @@ int lastUpdatedNode;
 int startingNode;
 
 void BellmanFord() {
-   // cout << "Inside function" << endl;
+ //   cout << "Inside function" << endl;
     for (int i = 0; i < n; i++) {
-       // cout << "Iteration " << i << " of loop 1" << endl;
-        dis[i] = 1e9;
-       // cout << dis[i] << " ";
+      //  cout << "Iteration " << i << " of loop 1" << endl;
+        dis[i] = 25e12;
     }
     dis[startingNode] = 0;
    // cout << dis[startingNode] << endl;
 
-   // cout << "About to enter loop 2" << endl;
+  //  cout << "About to enter loop 2" << endl;
     for (int iterations = 0; iterations < n - 1; iterations++) {
-      //  cout << "On iteration " << iterations << " with lastUpdatedNode of " << lastUpdatedNode << endl;
         lastUpdatedNode = -1; //resetting to see if the last updated node changes by the end of the iteration
         for (int i = 1; i <= n; i++) {
             for (auto itr : graph[i]) {
@@ -43,16 +42,31 @@ void BellmanFord() {
                 }
             }
         }
+      //  cout << "Finished iteration " << iterations << " with lastUpdatedNode of " << lastUpdatedNode << endl;
     }
 
     if (lastUpdatedNode != -1) {
         cout << "YES" << endl;
-        int currentNode = lastUpdatedNode;
-        while (parentNode[currentNode] != lastUpdatedNode) {
-            cout << parentNode[currentNode] << " ";
+        int startNode = 0;
+
+        startNode = lastUpdatedNode;
+        for (int i = 1; i <= n; i++) {
+            startNode = parentNode[startNode];
+        }
+
+        int currentNode = startNode;
+        while (parentNode[currentNode] != startNode) {
+            cyclePath.push_back(currentNode);
             currentNode = parentNode[currentNode];
         }
-        cout << lastUpdatedNode << endl;
+        cyclePath.push_back(currentNode);
+        cyclePath.push_back(startNode);
+
+        while (!cyclePath.empty()) {
+            cout << cyclePath.back() << " ";
+            cyclePath.pop_back();
+        }
+        cout << endl;
     } else {
         cout << "NO" << endl;
     }
@@ -63,17 +77,17 @@ void BellmanFord() {
 int main() {
     cin >> n >> m;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < m; i++) {
         int startNode;
         int endNode;
         int length;
 
+        cin >> startNode >> endNode >> length;
+        //cout << startNode << " " << endNode << " " << length << endl;
+
         if (i == 0) {
             startingNode = startNode;
         }
-
-        cin >> startNode >> endNode >> length;
-        cout << startNode << " " << endNode << " " << length << endl;
 
         graph[startNode].push_back({endNode, length});
 
@@ -82,7 +96,8 @@ int main() {
         length = 0;
     }
 
-    //cout << "Entering function" << endl;
+  //  cout << startingNode << endl;
+  //  cout << "Entering function" << endl;
     BellmanFord();
 
     return 0;
